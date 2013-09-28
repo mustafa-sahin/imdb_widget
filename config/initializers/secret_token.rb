162cbf0a1383065026acf97e91a60d2a72a225ab
Mustafa Sahin <mustafa.sahin@inspark.com>
2013-09-28 18:04:46 +0300

    project to Github

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IMDbWidget::Application.config.secret_token = '8b087f3fc7182aec3b9e9b0356bf324502a597dcdb228e8e3d5c5b296a1a8e8f9d4a192186fabe97a0fdf4230f36280c4e3f412ae6669848133799b3d16eb915'
