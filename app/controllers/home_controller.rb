9508cde9b1c46668903e673746405097a740262c
Mustafa Sahin <mustafa.sahin@inspark.com>
2013-09-29 14:37:03 +0300

    working on widget view and components is going on...

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,14 +5,27 @@ class HomeController < ApplicationController
   
   def find
     public_url = params[:address].empty? ? "http://www.imdb.com/user/ur27065058/ratings" : params[:address]
-    
-    @doc = Nokogiri::HTML(open(public_url))
-    @anchors = @doc.css("div .image").css("a").first(5)
-    @ttIds = []
-    
-    @anchors.each do |a|
-      @ttIds << a["href"].split("tt")[1].split("/")[0]
+    doc = Nokogiri::HTML(open(public_url))
+    ids = find_ids_by_type(doc)
+    @titles = []
+    ids.each do |title_id|
+      @titles << Imdb::Movie.new(title_id)
     end
     
   end
+  
+  def find_ids_by_type(doc)
+    arr = []
+    list_items = doc.css("div .list_item").first(50).each do |item|
+      a = item.css("div.info").css("div.episode")
+      if a.empty? # movie
+        to_parse = item.css("div.image").css("a").first
+        title_id = to_parse["href"].split("tt")[1].split("/")[0]
+        arr << title_id
+        return arr if arr.size == 5
+      end
+    end
+    arr
+  end
+  
 end
